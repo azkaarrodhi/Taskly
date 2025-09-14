b5090ab18c254ddad6514aac2758a2779f6b91b2
AzkaDev <azkaarrodhi11@gmail.com>
2025-09-14 08:51:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,42 @@
-const CACHE_NAME = "pwa-cache-v1";
+const CACHE_NAME = "pwa-cache-v2";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/styles.css",
   "/app.js",
-  "/taskly.png"
+  "/taskly.jpg",
+  "/manifest.json"
 ];
 
-// Install dan cache file
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-// Fetch: ambil dari cache kalau offline
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).then((res) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      });
     })
   );
 });
